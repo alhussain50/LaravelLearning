81f9780d1b48cf6a5d1a63554e51fa05c33a9666
Md Masuk Al Hussain <alhussain@iut-dhaka.edu>
2024-07-05 18:41:09 +0600

    view, data and routes

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -6,7 +6,9 @@
     <ul>
         @foreach ($jobs as $job)
             <li>
-                <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
+                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+                </a>
             </li>
         @endforeach
     </ul>
