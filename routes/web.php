2f1b473d54aab05b8a89a1c2ef8f741b2cec9a26
Md Masuk Al Hussain <alhussain@iut-dhaka.edu>
2024-07-12 19:56:04 +0600

    registration form

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\RegisteredController;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
 Route::resource('jobs', JobController::class);
+
+Route::get('/register', [RegisteredController::class, 'create']);
 
 Route::view('/contact', 'contact');
